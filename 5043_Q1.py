05ce088686545c57f299a306825d5a295a4eabb4
SanuviGamage5043 <sanuvigamage20842@gmail.com>
2026-03-25 10:16:24 +0530

    Q1 done

--- 5043_Q1.py
+++ 5043_Q1.py
@@ -1,20 +1,16 @@
 import cv2
 import matplotlib.pyplot as plt
 
-# Load image
 img = cv2.imread('Image_1.jpg')
 
 if img is None:
     print("Image not found!")
     exit()
 
-# Convert color
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-# Kernel sizes
 kernels = [3, 5, 11, 15]
 
-# Plot results
 plt.figure(figsize=(12,8))
 
 plt.subplot(2,3,1)
@@ -25,8 +21,7 @@ plt.axis('off')
 for i, k in enumerate(kernels):
     blurred = cv2.blur(img_rgb, (k, k))
 
-    # Save image
-    cv2.imwrite(f'avg_{k}.png', cv2.cvtColor(blurred, cv2.COLOR_RGB2BGR))
+    cv2.imwrite(f'Q1_avg_{k}.png', cv2.cvtColor(blurred, cv2.COLOR_RGB2BGR))
 
     plt.subplot(2,3,i+2)
     plt.imshow(blurred)
